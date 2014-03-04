712539d46953e6a59150cf447bbda529e2e65da8
Lars Greiss <kufikugel@googlemail.com>
2014-03-04 13:42:46 +0100

    Frameworks: hw key rebinding. Allow virtual keys to preload recents
    Apps like LMT or similar which send hw key events as virtual key
    cannot load new recents right now due of missing proper preload.
    Well it was before as well broken the old recents was just not that
    strict then the new one....but it should be due that this was one of the
    main reasons why the old one was buggy. Bad preloading
    
    whatever fix it and simply allow virtual keys which fire app switchter
    key to fire a recents preload.
    
    Change-Id: I09b96b50894af8a91c261e17c736a66ca377952d

--- policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
+++ policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
@@ -2552,6 +2552,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
             if (virtualKey && down) {
                 mAppSwitchPressed = true;
                 mAppSwitchConsumed = false;
+                preloadRecentApps();
                 return -1;
             }
 
